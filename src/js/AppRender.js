b791c24c3f9c51f43d8ef8f8c86477e4ef51ceca
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-04-07 23:43:20 +0300

    renderMessage method added

--- src/js/AppRender.js
+++ src/js/AppRender.js
@@ -6,9 +6,9 @@ export default class AppRender {
   init() {
     this.container = document.createElement('div');
     this.container.className = 'container';
-    this.renderTextarea();
-    this.container.append(this.mailerEl);
+    this.container.append(this.renderTextarea(), this.renderMessages());
     this.body.append(this.container);
+    this.renderMessage();
   }
 
   renderTextarea() {
@@ -22,5 +22,32 @@ export default class AppRender {
     this.mailerTextareaEl.name = 'message';
     this.mailerTextareaEl.placeholder = 'Ваше сообщение...';
     this.mailerEl.append(this.mailerTitleEl, this.mailerTextareaEl);
+    return this.mailerEl;
+  }
+
+  renderMessages() {
+    this.messagesEl = document.createElement('div');
+    this.messagesEl.className = 'messages';
+    this.incomingEl = document.createElement('p');
+    this.incomingEl.className = 'messages__incoming';
+    this.incomingEl.textContent = 'Incoming';
+    this.messagesEl.append(this.incomingEl);
+    return this.messagesEl;
+  }
+
+  renderMessage() {
+    this.messageEl = document.createElement('div');
+    this.messageEl.className = 'message';
+    this.contactEl = document.createElement('div');
+    this.contactEl.className = 'message__contact';
+    this.contactEl.textContent = 'dima@mishka';
+    this.textEl = document.createElement('div');
+    this.textEl.className = 'message__text';
+    this.textEl.textContent = 'Hello from Mishka';
+    this.timestampEl = document.createElement('div');
+    this.timestampEl.className = 'message__timestamp';
+    this.timestampEl.textContent = '23:38 07.04.2021';
+    this.messageEl.append(this.contactEl, this.textEl, this.timestampEl);
+    this.messagesEl.append(this.messageEl);
   }
 }
