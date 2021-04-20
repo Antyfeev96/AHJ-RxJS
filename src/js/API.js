28ef932a77f2b43fc54622c765318b76a85d9f6d
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-04-20 16:24:43 +0300

    Observer works now

--- src/js/API.js
+++ src/js/API.js
@@ -1,18 +1,16 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable consistent-return */
 /* eslint-disable no-return-assign */
-import { from } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
 export default class API {
   async initStream() {
-    this.stream$ = ajax.getJSON('https://ahj-rxjs-backend.herokuapp.com/messages/unread');
-    from(this.stream$).pipe(
+    this.unread$ = ajax.getJSON('https://ahj-rxjs-backend.herokuapp.com/messages/unread');
+    return interval(3000).pipe(
+      switchMap((intervalValue) => this.unread$),
       map((value) => value.messages),
-    ).subscribe((value) => {
-      this.response = value;
-    });
-    console.log(this.response); // значение записалось со второй
-    return this.response; // значение ушло
+    );
   }
 }
--- src/js/AppController.js
+++ src/js/AppController.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable consistent-return */
 /* eslint-disable no-return-assign */
 export default class AppController {
@@ -8,27 +9,38 @@ export default class AppController {
 
   async init() {
     this.layout.init();
-    this.messagesEl = document.querySelector('.messages');
-    this.initStream();
+    this.messagesEl = Array.from(document.querySelectorAll('.message'));
+    await this.initStream();
   }
 
   async initStream() {
-    setInterval(async () => {
-      while (this.messagesEl.lastChild && this.messagesEl.lastChild.nodeName === 'DIV') {
-        this.messagesEl.lastChild.remove();
-      }
-      this.messages = await this.takeResponse();
-      for (const message of this.messages) {
-        this.layout.renderMessage(message.from, message.subject, message.received);
-      }
-    }, 3000);
-  }
+    this.subscription$ = await this.api.initStream().then((stream) => {
+      stream.subscribe(async (messages) => {
+        while (document.querySelector('.message') !== null) {
+          document.querySelector('.message').remove();
+        }
+        for (const message of messages) {
+          this.layout.renderMessage(message.from, message.subject, message.received);
+        }
+        // if (stream) {
+        //   console.log(stream);
+        //   await stream.unsubscribe(); не работает, приложение не падает, ошибка unsubscribe is not a function
+        // }
+      });
+    });
+    // this.subscription$ = await this.api.initStream();
+    // this.subscription$.subscribe((messages) => {
+    //   while (document.querySelector('.message') !== null) {
+    //     document.querySelector('.message').remove();
+    //   }
 
-  async takeResponse() {
-    this.response = await this.api.initStream();
-    console.log(this.response); // значение записалось со второго раза
-    if (this.response) {
-      return this.response; // значение ушло
-    }
+    //   for (const message of messages) {
+    //     this.layout.renderMessage(message.from, message.subject, message.received);
+    //   }
+    // });
+    // if (this.subscription$) {
+    //   this.subscription$.unsubscribe();
+    //   не работает, приложение падает, unsubscribe is not a function;
+    // }
   }
 }
